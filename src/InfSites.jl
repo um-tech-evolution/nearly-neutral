aeb5b334c5ccbc8d67fd4ec6f6c1f15a663f3452
Alden Wright <github@fastmail.us>
2017-09-13 11:48:46 -0600

    Add src/InfSites.jl.

---
+++ src/InfSites.jl
@@ -0,0 +1,10 @@
+module InfSites
+include("../src/aliases.jl")
+include("../src/site_collection.jl")
+include("../src/nn_poplist.jl")
+include("../src/propsel.jl")
+include("../src/watterson.jl")
+include("../src/infsites.jl")
+end
+
+using InfSites
